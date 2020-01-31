2c8e68cfca077877e150a483877f7d3718225565
mochuan <sweetman108@outlook.com>
2020-02-01 05:13:38 +0800

    change config files

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(function (err, req, res, next) {
     res.json({error: err});
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(process.env.PORT || 5000, () => console.log('Listening on port 5000'));
 
 export default app;
 
